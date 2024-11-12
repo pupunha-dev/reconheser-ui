2d21e74d1968b6cfb90653d335497f89e0e10ea9
Pupunha Dev <pupunha.dev@gmail.com>
2024-11-12 09:22:22 -0300

    Custom colors criadas em ts.config. Adição de svgs na pasta public.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,14 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    colors: {
+      "rsa-amarelo": "#ffdf58",
+      "rsa-azul-1": "#03989e",
+      "rsa-azul-2": "#003c4f",
+      "rsa-cinza": "#f0f0f0",
+      "rsa-coral": "#ff6b6b"
+    }
+  },
   plugins: [],
 } satisfies Config;
